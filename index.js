4f3b011df21a8903f969d896764c2e6bec87d84a
Eugene Samoylenko <evgeniysamoylenko2000@gmail.com>
2020-06-17 23:27:09 +0300

    Add checkMultiplicity() function

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ console.log('isAdult(18) = ', isAdult(18));
 console.log('isAdult(15) = ', isAdult(15));
 console.log('isAdult(-23) = ', isAdult(-23));
 
+console.log('');
+
+console.log('checkMultiplicity(25, 5) = ', checkMultiplicity(25, 5));
+console.log('checkMultiplicity(25, 2) = ', checkMultiplicity(25, 2));
+console.log('checkMultiplicity(-15, -3) = ', checkMultiplicity(-15, -3));
 
 /**
  *
@@ -13,4 +18,14 @@ function isAdult(age) {
     return age >= 18;
   }
   return null;
+}
+
+/**
+ *
+ * @param {number} number
+ * @param {number} multiplicity
+ * @returns {boolean}
+ */
+function checkMultiplicity(number, multiplicity) {
+  return number % multiplicity === 0;
 }
